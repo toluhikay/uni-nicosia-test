51a23bcd66ed074db36e8af68cc83b07a4b5c04c
toluhikay <kayodesunday29@gmail.com>
2024-10-23 00:27:27 +0100

    fix: added close icon to mobile nav

--- app/components/SideBarMobile.tsx
+++ app/components/SideBarMobile.tsx
@@ -1,10 +1,19 @@
 import React from "react";
 import SideBarTop from "./SideBarTop";
 import SibeBarBottomLinks from "./SibeBarBottomLinks";
+import { CloseIcon } from "@/assets/svgs/DashboardSvgs";
 
 const SideBarMobile = ({ setModalOpen }: { setModalOpen: React.Dispatch<React.SetStateAction<boolean>> }) => {
   return (
     <aside className="h-full bg-primaryBlack w-[20rem]  p-5 text-primaryWhite border border-secondaryBlack md:hidden flex flex-col justify-between">
+      <div
+        className="absolute top-0 right-0 p-4 cursor-pointer"
+        onClick={() => {
+          setModalOpen(false);
+        }}
+      >
+        <CloseIcon />
+      </div>
       <SideBarTop setModalOpen={setModalOpen} />
       <SibeBarBottomLinks />
     </aside>
